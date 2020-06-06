3e05f00c01ad88a9ac019134faa0ec88c71e83c2
cheetosysst <thect@protonmail.com>
2020-06-06 22:51:51 +0800

    Added even more stuff
    some basic routing in ./index.js
    
    Added security.txt in public

---
+++ index.js
@@ -0,0 +1,42 @@
+// ./Index.js
+// ========================================
+// 程式的進入點，負責管理各個模組。請勿在這寫
+// 細節功能，將細節功能寫到別的地方再引入。
+// ========================================
+
+// Library
+const express 	= require('express');
+const app 		= express();
+const router 	= express.Router();
+const port    = process.env.PORT || 8086;
+// const favicon 	= require('serve-favicon');
+require('dotenv').config();
+
+// Router 
+
+// 	Print method on console
+router.use(function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
+
+// Homepage : 如題，是首頁
+router.get("/", function(req, res) {
+	res.send('home page!');
+});
+
+// About : 關於頁，目前只會引導到 Github
+router.get("/about", function(req, res) {
+	res.redirect(301, "https://github.com/cheetosysst/Eiki")
+});
+
+// Github : 引導到 Github
+router.get("/github", function(req, res) {
+	res.redirect(301, "https://github.com/cheetosysst/Eiki")
+});
+
+// Public : 用來提供靜態內容，像是 icon
+app.use(express.static("public"));
+
+app.use("/", router);
+app.listen(port);
