943a4065086655f3e4e1cfb1eb4cba70eeded0c7
cheetosysst <thect@protonmail.com>
2020-10-31 16:29:32 +0800

    Basic ejs templates

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express')
 const app = express()
+const path = require('path');
+app.set('views', path.join(__dirname, 'view'));
 
 require('dotenv').config()
 const port = process.env.port || 3000
 const siteName = process.env.name
 const version = require('../package').version;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index.ejs'))
 app.listen(port, () => console.log(`Eiki is now listening on port %d`, port))
