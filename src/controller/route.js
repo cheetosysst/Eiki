860a4e2e22f82a1e9749c5d0ec0d522b7c777ceb
cheetosysst <thect@protonmail.com>
2020-11-18 12:31:13 +0800

    submit page

--- src/controller/route.js
+++ src/controller/route.js
@@ -6,6 +6,10 @@ const strings = require("../model/lang/"+config.get("language")+".json")
 
 router.get('/', (req, res, next) => {
 	res.render('index', strings);
-});
+})
+
+router.get('/submit', (req, res, next) => {
+	res.render('submit', strings);
+})
 
 module.exports = router
